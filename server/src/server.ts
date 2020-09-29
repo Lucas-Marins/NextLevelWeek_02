aac31356e27eae917e3587eea39a4ded0767d082
Lucas Marins <lmarins02@hotmail.com.br>
2020-09-29 19:36:09 -0300

    Finished backend

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,12 +1,11 @@
 import express, { response } from 'express';
+import cors from 'cors'
+import routes from './routes';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json())
-
-app.post('/', (req,res)=>{
-    return response.json({message:'Hello world'})
-})
-
+app.use(routes)
 
 app.listen(3333);
